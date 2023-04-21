3a7290b2c8288ab6c4747ee6fe7fc1603d2823be
ErdemDalca <erdemdalca@gmail.com>
2023-04-21 12:34:00 +0300

    Menü Ekranı Tasarımı Değiştirildi

--- Ticaret Uygulaması/MenuScreen.Designer.cs
+++ Ticaret Uygulaması/MenuScreen.Designer.cs
@@ -28,82 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuScreen));
-            this.button1 = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.ekle = new System.Windows.Forms.Button();
-            this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(959, 648);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(190, 78);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "acilcikis";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MenuScreen));
+			this.button1 = new System.Windows.Forms.Button();
+			this.filtre = new System.Windows.Forms.ComboBox();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.ekle = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(959, 648);
+			this.button1.Margin = new System.Windows.Forms.Padding(4);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(190, 78);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "acilcikis";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// filtre
+			// 
+			this.filtre.BackColor = System.Drawing.Color.Lime;
+			this.filtre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.filtre.FormattingEnabled = true;
+			this.filtre.Items.AddRange(new object[] {
             "fiyata göre sırala",
             "eklenme tarihine göre sırala"});
-            this.comboBox1.Location = new System.Drawing.Point(12, 12);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(201, 24);
-            this.comboBox1.TabIndex = 1;
-            this.comboBox1.Text = "seç(sıralama kriterleri)";
-            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 126);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(940, 600);
-            this.flowLayoutPanel1.TabIndex = 2;
-            // 
-            // ekle
-            // 
-            this.ekle.Location = new System.Drawing.Point(847, 33);
-            this.ekle.Name = "ekle";
-            this.ekle.Size = new System.Drawing.Size(157, 54);
-            this.ekle.TabIndex = 3;
-            this.ekle.Text = "ekle";
-            this.ekle.UseVisualStyleBackColor = true;
-            this.ekle.Click += new System.EventHandler(this.ekle_Click);
-            // 
-            // MenuScreen
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::Ticaret_Uygulaması.Properties.Resources.mavi;
-            this.ClientSize = new System.Drawing.Size(1153, 741);
-            this.Controls.Add(this.ekle);
-            this.Controls.Add(this.flowLayoutPanel1);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4);
-            this.MinimizeBox = false;
-            this.Name = "MenuScreen";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Yilgin bir hosgoru ile benimsenmisler";
-            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.ResumeLayout(false);
+			this.filtre.Location = new System.Drawing.Point(12, 12);
+			this.filtre.Name = "filtre";
+			this.filtre.Size = new System.Drawing.Size(201, 24);
+			this.filtre.TabIndex = 1;
+			this.filtre.Text = "seç(sıralama kriterleri)";
+			this.filtre.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.AutoScroll = true;
+			this.flowLayoutPanel1.BackColor = System.Drawing.Color.Transparent;
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 42);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(940, 684);
+			this.flowLayoutPanel1.TabIndex = 2;
+			// 
+			// ekle
+			// 
+			this.ekle.Location = new System.Drawing.Point(959, 587);
+			this.ekle.Name = "ekle";
+			this.ekle.Size = new System.Drawing.Size(157, 54);
+			this.ekle.TabIndex = 3;
+			this.ekle.Text = "ekle";
+			this.ekle.UseVisualStyleBackColor = true;
+			this.ekle.Click += new System.EventHandler(this.ekle_Click);
+			// 
+			// MenuScreen
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.Green;
+			this.ClientSize = new System.Drawing.Size(1153, 741);
+			this.Controls.Add(this.ekle);
+			this.Controls.Add(this.flowLayoutPanel1);
+			this.Controls.Add(this.filtre);
+			this.Controls.Add(this.button1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(4);
+			this.MinimizeBox = false;
+			this.Name = "MenuScreen";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Yilgin bir hosgoru ile benimsenmisler";
+			this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+			this.ResumeLayout(false);
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox filtre;
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button ekle;
     }
